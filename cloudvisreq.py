a0a81e177dfb1acbea58437499379ed57b52fca8
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2017-12-17 03:26:25 +0530

    initial commit

---
+++ cloudvisreq.py
@@ -0,0 +1,83 @@
+from base64 import b64encode
+from os import makedirs
+from os.path import join, basename
+from sys import argv
+import json
+import requests
+import env
+
+ENDPOINT_URL = 'https://vision.googleapis.com/v1/images:annotate'
+RESULTS_DIR = 'jsons'
+try:
+    makedirs(RESULTS_DIR)
+except:
+    pass
+
+def make_image_data_list(image_filenames):
+    """
+    image_filenames is a list of filename strings
+    Returns a list of dicts formatted as the Vision API
+        needs them to be
+    """
+    img_requests = []
+    for imgname in image_filenames:
+        with open(imgname, 'rb') as f:
+            ctxt = b64encode(f.read()).decode()
+            img_requests.append({
+                    'image': {'content': ctxt},
+                    'features': [{
+                        'type': 'DOCUMENT_TEXT_DETECTION',
+                        'maxResults': 1
+                    }]
+            })
+    return img_requests
+
+def make_image_data(image_filenames):
+    """Returns the image data lists as bytes"""
+    imgdict = make_image_data_list(image_filenames)
+    return json.dumps({"requests": imgdict }).encode()
+
+
+def request_ocr(api_key, image_filenames):
+    response = requests.post(ENDPOINT_URL,
+                             data=make_image_data(image_filenames),
+                             params={'key': api_key},
+                             headers={'Content-Type': 'application/json'})
+    return response
+
+
+def get_lines(image_filenames=["ocr1.png"]):
+    #image_filenames = ["ocr1.png"]
+    api_key = env.api_key
+    if not api_key or not image_filenames:
+        print("""
+            Please supply an api key, then one or more image filenames
+            $ python cloudvisreq.py api_key image1.jpg image2.png""")
+    else:
+        response = request_ocr(api_key, image_filenames)
+        if response.status_code != 200 or response.json().get('error'):
+            #print(response.text)
+            pass
+        else:
+            for idx, resp in enumerate(response.json()['responses']):
+                # save to JSON file
+                imgname = image_filenames[idx]
+                jpath = join(RESULTS_DIR, basename(imgname) + '.json')
+                with open(jpath, 'w') as f:
+                    datatxt = json.dumps(resp, indent=2)
+                    #print("Wrote", len(datatxt), "bytes to", jpath)
+                    #f.write(datatxt)
+
+                # print the plaintext to screen for convenience
+                #print("---------------------------------------------")
+                #print 'response:\n', resp, "\n\n\n"
+
+                try:
+                    t = resp['textAnnotations'][0]
+                except:
+                    return
+                #print("    Bounding Polygon:")
+                #print(t['boundingPoly'])
+                #print("    Text:")
+                #print(t['description'])
+                return t['description']
---
+++ template_match.py
@@ -0,0 +1,81 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import imutils
+
+
+
+def find_maxVal(gray, template_path):
+	visualize = True
+	found = None
+	template = cv2.imread(template_path)
+	template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+	(tH, tW) = template.shape[:2]
+	template = cv2.Canny(template, 50, 200)
+	
+
+	# loop over the scales of the image
+	for scale in np.linspace(0.2, 1.0, 20)[::-1]:
+		# resize the image according to the scale, and keep track
+		# of the ratio of the resizing
+		resized = imutils.resize(gray, width = int(gray.shape[1] * scale))
+		r = gray.shape[1] / float(resized.shape[1])
+
+		# if the resized image is smaller than the template, then break
+		# from the loop
+		if resized.shape[0] < tH or resized.shape[1] < tW:
+			break
+
+		edged = cv2.Canny(resized, 200, 200)
+		result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
+		(_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+
+		# check to see if the iteration should be visualized
+		if visualize == False:
+			# draw a bounding box around the detected region
+			clone = np.dstack([edged, edged, edged])
+			cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
+				(maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+			cv2.imshow("Visualize", clone)
+			cv2.waitKey(0)
+
+		# if we have found a new maximum correlation value, then ipdate
+		# the bookkeeping variable
+		if found is None or maxVal > found[0]:
+			found = (maxVal, maxLoc, r)
+	return found,(tH, tW)
+
+
+def crop_text(fname):
+	image = cv2.imread(fname)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+	found_hor = find_maxVal(gray, 'data/template_horizontal.jpg')
+	found_ver = find_maxVal(gray, 'data/template_vertical.jpg')
+
+	verti = True
+	if found_ver[0][0] >= found_hor[0][0]:
+		found = found_ver[0]
+		(tH, tW) = found_ver[1]
+		verti = True
+	else:
+		found = found_hor[0]
+		(tH, tW) = found_hor[1]
+		print 'horizontal'
+	# unpack the bookkeeping varaible and compute the (x, y) coordinates
+	# of the bounding box based on the resized ratio
+	(_, maxLoc, r) = found
+	(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+	(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+
+	# draw a bounding box around the detected result and display the image
+	#cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+	#cv2.imshow("Image", image)
+	if verti == True:
+		imCrop = image[int(startY):int(startY+endY), int(startX):int(startX+endX)]
+	else:
+		#imCrop = image[int(startY):int(startY+endY), int(startX):int(startX+endX)]
+		imCrop = image[int(startX):int(endX), int(startY):int(endY)]
+	f, ext = fname.split('.')
+    new_name = f + '_cropped.' + ext
+	cv2.imwrite(new_name, imCrop)
