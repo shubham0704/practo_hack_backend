4e0045056184190526a5f891ec7009125b4143e4
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2017-12-17 14:46:19 +0530

    semi changes

--- controllers/get_data.py
+++ controllers/get_data.py
@@ -1,8 +1,8 @@
 from controllers.modules import *
 #from modules import *
 from controllers import cloudvisreq
-#import cloudvisreq
-
+import cloudvisreq
+MON = ["JAN", "FEB", "MAR", "MAY", "JUN", "JUNE", "JULY",  "JUL", "AUG", "SEPT","SEP", "OCT", "NOV", "DEC"]
 
 def preprocess(fname):
 	# TODO resize image
@@ -29,25 +29,42 @@ def sanitize_data(fname, prepro=False, vertical=False):
 		data = cloudvisreq.get_lines(image_filenames=[fname])
 	
 	#data = unicodedata.normalize('NFKD', data).encode('ascii','ignore')
-	lines = data.split('\n')
+	try:
+		lines = data.split('\n')
+		#print(lines)
+		#print(data)
+	except:
+		return
 	dates = []
-	
+	print lines
 	for line in lines:
+		try:
+			dt = re.search("EXP.[\s]{0,1}([A-Z]{3,4}[\s]{0,1}.[0-9]{2,4})", line).group(1)
+			dates.append(dt)
+		except:
+			pass
 		items = line.split()
 		if len(items) > 1:
 			for item in items:
 				try:
-					if re.match('([0-9].+)/([0-9].+)', item) or re.match('([a-zA-Z].+).[0-9].+', item):
+					if re.match('([0-9].+)/([0-9].+)', item) or re.match('([a-zA-Z].+)([.\w])[0-9].+', item):
 						now = parse(item)
 						dates.append(now)
 				except Exception as e:
 					
 					continue
+			try:
+				dt = re.search("[A-Z]{3}.[\s]{0,1}[0-9]{2,4}", line).group(0)
+				dates.append(dt)
+			except:
+				pass
 		else:
 			try:
 				if re.match('([0-9].+)/([0-9].+)', line) or re.match('([a-zA-Z].+).[0-9].+', line):	
-					now = parse(lines)
+					now = parse(line)
 					dates.append(now)
+				else:
+					pass
 			except:
 				pass
 	
@@ -62,12 +79,58 @@ def sanitize_data(fname, prepro=False, vertical=False):
 					dates.append(date)
 				except:
 					pass
+		#print line
+		try:
+			dt = re.search("EXP.[\s]{0,1}([A-Z]{3,4}[\s]{0,1}.[0-9]{2,4})", line).group(1)
+			dates.append(dt)
+		except:
+			pass
 	try:
 		#print vertical
+		print ('dates:', dates)
+		#dates = [parse(dt) for dt in dates]
 		return max(dates)
 
 	except Exception as e:
-		return None
+		nd = []
+		if dates:	
+			for dt in dates:
+				try:
+					x = parse(dt)
+					nd.append(x)
+				except:
+					try:
+						#dt = " ".(it for it in dt.split("."))
+						its = dt.split(".")
+						# if its[0] not in MON:
+						# 	for it in MON:
+						# 		if its[0][:1] == it[:1]:
+						# 			its[0] == it
+						# dt = " ".join((it for it in dt.split(".")))
+						# x = parse(dt)
+						# nd.append(x)
+						if its[0] == "SER":
+							its[0] = "SEP"
+							its[1] = "20" + its[1] 
+						dt = ".".join((it for it in its))
+						print (dt)
+						nd.append(parse(dt))
+					except:
+						pass
+
+			#dates = [parse(dt) for dt in dates]
+			try:
+				filt = []	
+				for it in nd:
+					if type(it) == type("av"):
+						continue
+					filt.append(it)
+				return max(filt)
+			except:
+				print(nd)
+				return None
+		else:
+			return None
 
 	return 
 
@@ -108,8 +171,7 @@ def get_name(fname):
 		return k 
 	return k
 
+if __name__ == '__main__':
 
-# if __name__ == '__main__':
-
-# 	#print get_name('/home/master/Desktop/GIT/practo_hack_backend/uploads/temp/bc228d1a-8dd0-4447-ba61-95fc05a287e7.jpg')
-# 	print sanitize_data('/home/master/Desktop/GIT/practo_hack_backend/uploads/temp/bc228d1a-8dd0-4447-ba61-95fc05a287e7.jpg')
+ #print get_name('/home/master/Desktop/GIT/practo_hack_backend/uploads/temp/bc228d1a-8dd0-4447-ba61-95fc05a287e7.jpg')
+ print sanitize_data('/home/master/Desktop/GIT/practo_hack_backend/uploads/temp/IMG_20171217_131015.jpg')
